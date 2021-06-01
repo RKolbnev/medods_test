0c3df6674319475cc1f7c363f9d0d5bcec8d0c35
RKolbnev <RKolbnev>
2021-06-01 18:31:45 +0300

    _

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' ? '/medods_test/' : '/'
+  lintOnSave: false
+  // publicPath: process.env.NODE_ENV === 'production' ? '/medods_test/' : '/'
+  // publicPath: '/medods_test/'
 }
